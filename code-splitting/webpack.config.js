9eae5a7abd51e006bd1807f6abdc6a5e9f85ebfd
bbaing4564 <bbaing4564@gmail.com>
2020-04-03 14:21:18 +0900

    code splitting training

---
+++ code-splitting/webpack.config.js
@@ -0,0 +1,24 @@
+let path = require('path');
+let MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: 'none',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {loader: MiniCssExtractPlugin.loader },
+           'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin()
+  ],
+};
